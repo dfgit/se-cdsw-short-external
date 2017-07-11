3d20b05922af7fae21e769b8321b4b3c981e2f05
Dan Flavin <dflav@comcast.net>
2017-07-11 16:54:24 -0500

    Update setup_R_pkgs.R

--- SETUP/setup_R_pkgs.R
+++ SETUP/setup_R_pkgs.R
@@ -8,21 +8,20 @@ setupInstallLoadPkgs <- function(pkgList)
 #  options(repos = c(repo.data.frame$URL, 'https://cran.rstudio.org'))
 
   print(pkgList)
-<<<<<<< HEAD
+
   pkgsToLoad <- pkgList[!(pkgList %in% installed.packages()[,"Package"])];
-=======
-  pkgsToLoad <- pkgList[!(pkgList %in% installed.packages(lib.loc='/home/cdsw/R')[,"Package"])];
->>>>>>> 5c0731f89204535ab85049bab1b1437a6e99bb8b
+
+#  pkgsToLoad <- pkgList[!(pkgList %in% installed.packages(lib.loc='/home/cdsw/R')[,"Package"])];
+
 
   if(length(pkgsToLoad)) {
     startTime <- proc.time()
     install.packages(pkgsToLoad, dependencies = TRUE,
-<<<<<<< HEAD
-                      repos=c('http://cran.us.r-project.org', 'https://cran.rstudio.org'));
-=======
-                     lib='/home/cdsw/R',
                      repos=c('http://cran.us.r-project.org', 'https://cran.rstudio.org'));
->>>>>>> 5c0731f89204535ab85049bab1b1437a6e99bb8b
+
+#                     lib='/home/cdsw/R',
+#                     repos=c('http://cran.us.r-project.org', 'https://cran.rstudio.org'));
+
   }
 
   for(package_name in pkgList) {
